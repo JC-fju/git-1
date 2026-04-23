cde1272a327a44bfe93d29eedcf5a78720cb86fb
JC-fju <414170443@m365.fju.edu.tw>
2026-04-23 12:54:44 +0800

    0423-8

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=true, port=5000)
+    app.run(host="0.0.0.0",debug=True, port=5000)
