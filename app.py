5c706c872bc754942228c255673d213c81b7ec4d
JC-fju <414170443@m365.fju.edu.tw>
2026-05-07 12:32:31 +0800

    jinja

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name="Jerry")
 
 @app.route("/html_tags")
 def html_tags():
     return render_template("html_tags.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True, port=5000)
