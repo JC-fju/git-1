78ee63f87a8f01cb934d043fede8d70fc657c3a7
JC-fju <414170443@m365.fju.edu.tw>
2026-05-14 12:29:15 +0800

    0514TA

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    content = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
@@ -13,6 +34,31 @@ def html_tags():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/todo")
+def todo():
+
+    todos = Todo.query.all()
+
+    return render_template(
+        "todo.html",
+        todos=todos
+    )
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+
+    content = request.form.get("content")
+
+    if content:
+
+        new_todo = Todo(content=content)
+
+        db.session.add(new_todo)
+
+        db.session.commit()
+
+    return redirect("/todo")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True, port=5000)
